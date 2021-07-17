b13fbd4e08906eb4bbca5550c5b948dc5afd158b
thesloppyguy <sahilmandal@outlook.com>
2021-07-17 15:02:38 +0530

    PROG

--- Array/arrayop.cpp
+++ Array/arrayop.cpp
@@ -13,25 +13,72 @@ void init(int x, int y, int z)
     arr.p = new int(x);
     arr.length = y;
     arr.size = z;
+    for (int i = 0; i < arr.size; i++)
+    {
+        arr.p[i] = '\0';
+    }
 }
 
 void insert(int k)
 {
     if (arr.length == arr.size)
     {
-        cout<<"array limit reached delete somethings"<<endl;
+        cout << "array limit reached delete somethings" << endl;
+    }
+    else
+    {
+        cout << "inserted" << k << endl;
+        arr.p[arr.length] = k;
+        arr.length++;
     }
 }
 
 void insert(int k, int pos)
 {
+    if (arr.length == arr.size)
+    {
+        cout << "array limit reached delete somethings" << endl;
+    }
+    else if (arr.p[pos - 1] == '\0')
+    {
+        cout << "inserted" << k << endl;
+        arr.p[pos - 1] = k;
+        arr.length++;
+    }
+    else
+    {
+        for (int i = arr.length; i >= pos; i--)
+        {
+            arr.p[i + 1] = arr.p[i];
+        }
+        cout << "inserted" << k << endl;
+        arr.p[pos - 1] = k;
+        arr.length++;
+    }
+}
+// void del()
+// {
+// }
+// void del(int x, int pos)
+// {
+// }
 
-}
-void del()
+void display()
 {
-}
-void del(int x, int pos)
-{
+    if (arr.length == 0)
+    {
+        cout << "the array is empty" << endl;
+    }
+    else
+    {
+        for (int i = 0; i != arr.size; i++)
+        {
+            if (arr.p[i] != '\0')
+            {
+                cout << arr.p[i] << endl;
+            }
+        }
+    }
 }
 
 int main()
@@ -39,6 +86,7 @@ int main()
     init(10, 0, 10);
     insert(10);
     insert(20);
-    del();
+    insert(30, 5);
+    display();
     return 0;
 }
